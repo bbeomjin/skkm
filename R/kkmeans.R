57ca1dc8382aa87dceeec4b036c243f42cb4a75e
unknown <bbbeomjin@gmail.com>
2022-10-06 20:04:19 +0900

    add tuning function for kkmeans

--- R/kkmeans.R
+++ R/kkmeans.R
@@ -1,57 +1,57 @@
-# #' tune.skkm
-# #' 
-# #' tuning kkmeans parameter
-# #' @importFrom parallel mclapply
-# #' @importFrom kernlab kkmeans
-# #' @param x input data
-# #' @return out results of kkmeans
-# #' @export 
-# tune.kkmeans = function(x, nCluster, nPerms = 20, nStart = 10, weights = NULL,
-#                         kernel = "linear", kparam = 1, opt = TRUE, nCores = 1, ...)
-# {
-#   out = list()
-#   call = match.call()
-#   kernel = match.arg(kernel, c("linear", "gaussian"))
-#   
-#   x = as.matrix(x)
-#   n = nrow(x)
-# 
-#   perm_list = vector("list", nPerms)
-#   for (i in 1:nPerms) {
-#     perm_list[[i]] = sapply(1:p, function(j) sample(x[, j]))
-#   }
-# 
-#   org_bcd = unlist(parallel::mclapply(1:length(kparam), FUN = function(j) {
-#     org_fit = kkmeans(x = x, nCluster = nCluster, nStart = nStart, weights = weights,
-#                       kernel = kernel, kparam = kparam[j], opt = TRUE, ...)
-#     return(org_fit$maxBcd)
-#   }, mc.cores = nCores))
-# 
-# 
-#   perm_bcd_list = matrix(0, nrow = nPerms, ncol = length(kparam))
-#   for (b in 1:nPerms) {
-#     perm_bcd = unlist(parallel::mclapply(1:length(kparam), FUN = function(j) {
-#       perm_fit = kkmeans(x = perm_list[[b]], nCluster = nCluster, nStart = nStart, weights = weights,
-#                          kernel = kernel, kparam = kparam[j], opt = TRUE, ...)
-#       return(perm_fit$maxBcd)
-#     }, mc.cores = nCores))
-#     
-#     perm_bcd_list[b, ] = perm_bcd
-#   }
-#   out$orgBcd = org_bcd
-#   out$permBcd = perm_bcd_list
-#   out$gaps = log(org_bcd) - colMeans(log(perm_bcd_list))
-#   out$optInd = min(which(out$gaps == max(out$gaps)))
-#   out$opt_kparam = kparam[out$optInd]
-#   
-#   if (opt) {
-#     opt_fit = kkmeans(x = x, nCluster = nCluster, nStart = nStart, weights = weights,
-#                       kernel = kernel, kparam = out$opt_kparam, opt = TRUE, ...)  
-#     out$optModel = opt_fit
-#   }
-#   out$call = call
-#   return(out)
-# }
+#' tune.skkm
+#' 
+#' tuning kkmeans parameter
+#' @importFrom parallel mclapply
+#' @importFrom kernlab kkmeans
+#' @param x input data
+#' @return out results of kkmeans
+#' @export 
+tune.kkmeans = function(x, nCluster, nPerms = 20, nStart = 10, weights = NULL,
+                        kernel = "linear", kparam = 1, opt = TRUE, nCores = 1, ...)
+{
+  out = list()
+  call = match.call()
+  kernel = match.arg(kernel, c("linear", "gaussian"))
+  
+  x = as.matrix(x)
+  n = nrow(x)
+
+  perm_list = vector("list", nPerms)
+  for (i in 1:nPerms) {
+    perm_list[[i]] = sapply(1:p, function(j) sample(x[, j]))
+  }
+
+  org_bcd = unlist(parallel::mclapply(1:length(kparam), FUN = function(j) {
+    org_fit = kkmeans(x = x, nCluster = nCluster, nStart = nStart, weights = weights,
+                      kernel = kernel, kparam = kparam[j], opt = TRUE, ...)
+    return(org_fit$maxBcd)
+  }, mc.cores = nCores))
+
+
+  perm_bcd_list = matrix(0, nrow = nPerms, ncol = length(kparam))
+  for (b in 1:nPerms) {
+    perm_bcd = unlist(parallel::mclapply(1:length(kparam), FUN = function(j) {
+      perm_fit = kkmeans(x = perm_list[[b]], nCluster = nCluster, nStart = nStart, weights = weights,
+                         kernel = kernel, kparam = kparam[j], opt = TRUE, ...)
+      return(perm_fit$maxBcd)
+    }, mc.cores = nCores))
+    
+    perm_bcd_list[b, ] = perm_bcd
+  }
+  out$orgBcd = org_bcd
+  out$permBcd = perm_bcd_list
+  out$gaps = log(org_bcd) - colMeans(log(perm_bcd_list))
+  out$optInd = min(which(out$gaps == max(out$gaps)))
+  out$opt_kparam = kparam[out$optInd]
+  
+  if (opt) {
+    opt_fit = kkmeans(x = x, nCluster = nCluster, nStart = nStart, weights = weights,
+                      kernel = kernel, kparam = out$opt_kparam, opt = TRUE, ...)  
+    out$optModel = opt_fit
+  }
+  out$call = call
+  return(out)
+}
 
 #' kkmeans
 #' 
